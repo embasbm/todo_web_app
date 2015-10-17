d2cd2f09222677f0ff7beaacdca7c57e82ab3d25
Emba Moussa <emoussa@intellicentrics.com>
2015-10-17 17:38:47 +0100

    exercise finished

--- todo_app.rb
+++ todo_app.rb
@@ -9,6 +9,13 @@ class Task
   property :created_at, DateTime
 end
 
+class MeetupGroup
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, Text
+end
+
+
 DataMapper.finalize.auto_upgrade!
 
 class TodoApp < Sinatra::Base
@@ -19,6 +26,25 @@ class TodoApp < Sinatra::Base
   end
 
   # example route, access this with http://localhost:9292/example
+  get '/meetup_groups' do
+    api_result = RestClient.get 'http://api.meetup.com/groups.json/?&topic=ruby&order=members'
+    jhash = JSON.parse(api_result)
+    counter = jhash['results'].count
+    output = ''
+    jhash['results'].each do |j|
+      name = j['name']
+      city = j['city']
+      focus = j['who']
+      count = j['members']
+      contact = j['organizer_name']
+      link = j['link']
+      country = j['country']
+      output << "&lt;tr&gt;&lt;td&gt;#{name}&lt;/td&gt; &lt;td&gt;&lt;a href = '#{link}' target = _new&gt;#{city}&lt;/a&gt;&lt;/td&gt;&lt;td&gt;#{country.upcase}&lt;/td&gt;&lt;td&gt;#{focus}&lt;/td&gt; &lt;td&gt;#{count}&lt;/td&gt;&lt;td&gt;#{contact}&lt;/td&gt;&lt;/tr&gt;"
+    end
+
+    erb :meetup_table, :locals => {result: output, counter: counter}
+  end
+
   get '/todos' do
     @tasks = Task.all(order: :created_at.desc)
     erb :todos
